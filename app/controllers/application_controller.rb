4f627b9160d9479379405540c85abc8aec070ec4
eddev <junjunpojr@gmail.com>
2026-07-02 21:45:46 +0800

    refactor: simplify after_sign_in_path_for method by using stored_location_for

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -46,11 +46,12 @@ class ApplicationController < ActionController::Base
   protected
 
     def after_sign_in_path_for(resource)
-      if current_user.admin?
-        admin_root_path
-      else
-        root_path
-      end
+      stored_location_for(resource) ||
+        if resource.admin?
+          admin_root_path
+        else
+          root_path
+        end
     end
 
     def after_sign_out_path_for(resource_or_scope)
