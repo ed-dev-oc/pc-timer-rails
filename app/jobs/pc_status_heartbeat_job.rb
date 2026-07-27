f1bda6f1b9d323d6c4ac7ce734d9f53c9186beff
eddev <junjunpojr@gmail.com>
2026-07-27 16:25:15 +0800

    fix(pc_status_heartbeat_job): mark online PCs offline when they exceed the heartbeat threshold

--- app/jobs/pc_status_heartbeat_job.rb
+++ app/jobs/pc_status_heartbeat_job.rb
@@ -2,8 +2,11 @@ class PcStatusHeartbeatJob < ApplicationJob
   queue_as :default
 
   def perform
-    Pc.where("last_seen_at < ?", Setting.integer("pc_offline_threshold").minutes.ago).find_in_batches(batch_size: 100) do |batch|
+    Pc.where(status: :online)
+      .where("last_seen_at < ?", Setting.integer("pc_offline_threshold").minutes.ago)
+      .find_in_batches(batch_size: 100) do |batch|
       batch.each do |pc|
+        pc.offline!
         Pcs::Broadcasts::BadgeStatus.call(pc)
       end
     end
