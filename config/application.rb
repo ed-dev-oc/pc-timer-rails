9371dc6ca81bb5e29c40970f08def1bc90934d63
eddev <junjunpojr@gmail.com>
2026-08-10 16:39:30 +0800

    refactor(config): move Active Record encryption settings from initializer to application.rb

--- config/application.rb
+++ config/application.rb
@@ -29,5 +29,9 @@ module PcTimerRails
 
     config.mission_control.jobs.base_controller_class = "Admin::BaseController"
     config.mission_control.jobs.http_basic_auth_enabled = false
+
+    config.active_record.encryption.primary_key = ENV.fetch("ACTIVE_RECORD_ENCRYPTION_PRIMARY_KEY")
+    config.active_record.encryption.deterministic_key = ENV.fetch("ACTIVE_RECORD_ENCRYPTION_DETERMINISTIC_KEY")
+    config.active_record.encryption.key_derivation_salt = ENV.fetch("ACTIVE_RECORD_ENCRYPTION_KEY_DERIVATION_SALT")
   end
 end
