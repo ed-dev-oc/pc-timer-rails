239e9a0533b3a38fe44067c280bdba34325c60d3
eddev <junjunpojr@gmail.com>
2026-08-17 02:26:54 +0800

    refactor(broadcasts): replace broadcast_replace_to with broadcast_replace_later_to in CoinTransactions::BroadcastService

--- app/services/coin_transactions/broadcast_service.rb
+++ app/services/coin_transactions/broadcast_service.rb
@@ -3,7 +3,7 @@ module CoinTransactions
     include ActionView::RecordIdentifier
 
     def self.call(pc)
-      pc.broadcast_replace_to(
+      pc.broadcast_replace_later_to(
         "coin_transaction",
         target: ActionView::RecordIdentifier.dom_id(pc, :inserted_amount),
         html: ApplicationController.render(
