74ced055ec29b4a96e45ba3b0e22ee3e22b2c033
eddev <junjunpojr@gmail.com>
2026-08-20 21:05:19 +0800

    feat(admin/pcs): add SessionPanelComponent and update view references to use it

---
+++ app/components/admin/pcs/session_panel_component.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+class Admin::Pcs::SessionPanelComponent < ViewComponent::Base
+  def initialize(pc:)
+    @pc = pc
+  end
+
+  private
+
+  attr_reader :pc
+
+  def pc_session
+    @pc.active_session
+  end
+end
